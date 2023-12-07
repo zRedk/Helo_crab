9bc5f14a94b885c7b79d1d8060a10a9e21556c6b
AldoVitolo17 <aldo.vitolo1910@gmail.com>
2023-12-07 17:41:45 +0100

    prova

--- Helo_crab/GameScene.swift
+++ Helo_crab/GameScene.swift
@@ -10,8 +10,27 @@ import SpriteKit
 
 class GameScene: SKScene{
     
+    let background = SKSpriteNode(imageNamed: "Background")
+    let player = SKSpriteNode(imageNamed: "Image")
+    let ground = SKSpriteNode(imageNamed: "Platform")
+    let platform = SKSpriteNode(imageNamed: "Platform")
+    
     override func didMove(to view: SKView) {
-        backgroundColor = .blue
+        self.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        
+        self.anchorPoint = .zero
+        
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.zPosition = 1
+        background.setScale(2.5)
+        addChild (background)
+        
+        ground.position = CGPoint(x: size.width / 2, y: 10)
+        ground.zPosition = 5
+        ground.setScale (15)
+        addChild(ground)
+        
+        
     }
     
     
