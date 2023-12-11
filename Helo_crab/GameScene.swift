39902d12f2fde88c0e3ac49b2403f85a226af6c5
AldoVitolo17 <aldo.vitolo1910@gmail.com>
2023-12-11 16:33:48 +0100

    working version of platform generating

--- Helo_crab/GameScene.swift
+++ Helo_crab/GameScene.swift
@@ -1,10 +1,3 @@
-//
-//  GameScene.swift
-//  Helo_crab
-//
-//  Created by Federica Mosca on 07/12/23.
-//
-
 import SpriteKit
 import SwiftUI
 import GameplayKit
@@ -23,10 +16,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         case platform = 0b10
     }
     
-    override func didMove(to view: SKView){
-        
+    var lastGeneratedPlatformY: CGFloat = 0.0
+    
+    override func didMove(to view: SKView) {
         self.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        
         self.anchorPoint = .zero
         
         background.position = CGPoint(x: size.width / 2, y: 500)
@@ -49,7 +42,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.zPosition = 10
         player.setScale(0.5)
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.height / 2)
-        player.physicsBody?.isDynamic = false //later is true
+        player.physicsBody?.isDynamic = false
         player.physicsBody?.restitution = 1
         player.physicsBody?.friction = 0
         player.physicsBody?.angularDamping = 0
@@ -59,43 +52,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.camera = cameraNode
         addChild(cameraNode)
-        
         cameraNode.position = player.position
-        
         addChild(player)
         
+        // Imposta l'ultima piattaforma generata inizialmente
+        lastGeneratedPlatformY = player.position.y
+        
+        // Genera le prime 6 piattaforme
         makePlatform()
         makePlatform2()
         makePlatform3()
         makePlatform4()
         makePlatform5()
         makePlatform6()
-
-        
     }
+    
     override func update(_ currentTime: TimeInterval) {
         cameraNode.position.y = player.position.y
+        
+        // Se il giocatore ha sbloccato una nuova parte dello schermo, genera una nuova piattaforma
+        if player.position.y > lastGeneratedPlatformY - size.height {
+            generateNewPlatform()
+        }
     }
-    
-    
     
     func didBegin(_ contact: SKPhysicsContact) {
         let contactA: SKPhysicsBody
         let contactB: SKPhysicsBody
         
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-            contactA = contact.bodyA // player
-            contactB = contact.bodyB //platform
+            contactA = contact.bodyA
+            contactB = contact.bodyB
         } else {
-            contactA = contact.bodyB //player
-            contactB = contact.bodyA //platform
+            contactA = contact.bodyB
+            contactB = contact.bodyA
         }
         if contactA.categoryBitMask == bitmasks.player.rawValue && contactB.categoryBitMask == bitmasks.platform.rawValue {
-            
+            // Aumenta la dimensione del salto del giocatore
             if player.physicsBody!.velocity.dy < 0 {
-                player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 700)
-                makePlatform5()
-                makePlatform6()
+                player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 1000)
             }
         }
     }
@@ -103,7 +98,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            
             player.position.x = location.x
         }
     }
@@ -116,11 +110,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         firstTouch = true
-        
     }
+    
     func makePlatform() {
         let platform = SKSpriteNode(imageNamed: "Platform")
         platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: 70, highestValue: 150).nextInt() + Int(player.position.y))
+        platform.setScale(2)
         platform.zPosition = 5
         platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
         platform.physicsBody?.isDynamic = false
@@ -131,10 +126,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
         addChild(platform)
     }
+    
     func makePlatform2() {
         let platform = SKSpriteNode(imageNamed: "Platform")
         platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: 200, highestValue: 300).nextInt() + Int(player.position.y))
         platform.zPosition = 5
+        platform.setScale(2)
         platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
         platform.physicsBody?.isDynamic = false
         platform.physicsBody?.allowsRotation = false
@@ -144,49 +141,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
         addChild(platform)
     }
+    
     func makePlatform3() {
-        let platform = SKSpriteNode(imageNamed: "Platform")
-        platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: 350, highestValue: 450).nextInt() + Int(player.position.y))
-        platform.zPosition = 5
-        platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
-        platform.physicsBody?.isDynamic = false
-        platform.physicsBody?.allowsRotation = false
-        platform.physicsBody?.affectedByGravity = false
-        platform.physicsBody?.categoryBitMask = bitmasks.platform.rawValue
-        platform.physicsBody?.collisionBitMask = 0
-        platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
+        let platform = generatePlatform(minY: Int(player.position.y) + 350, maxY: Int(player.position.y) + 450)
         addChild(platform)
     }
+    
     func makePlatform4() {
-        let platform = SKSpriteNode(imageNamed: "Platform")
-        platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: 500, highestValue: 600).nextInt() + Int(player.position.y))
-        platform.zPosition = 5
-        platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
-        platform.physicsBody?.isDynamic = false
-        platform.physicsBody?.allowsRotation = false
-        platform.physicsBody?.affectedByGravity = false
-        platform.physicsBody?.categoryBitMask = bitmasks.platform.rawValue
-        platform.physicsBody?.collisionBitMask = 0
-        platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
+        let platform = generatePlatform(minY: Int(player.position.y) + 500, maxY: Int(player.position.y) + 600)
         addChild(platform)
     }
+    
     func makePlatform5() {
-        let platform = SKSpriteNode(imageNamed: "Platform")
-        platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: 650, highestValue: 750).nextInt() + Int(player.position.y))
-        platform.zPosition = 5
-        platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
-        platform.physicsBody?.isDynamic = false
-        platform.physicsBody?.allowsRotation = false
-        platform.physicsBody?.affectedByGravity = false
-        platform.physicsBody?.categoryBitMask = bitmasks.platform.rawValue
-        platform.physicsBody?.collisionBitMask = 0
-        platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
+        let platform = generatePlatform(minY: Int(player.position.y) + 650, maxY: Int(player.position.y) + 750)
         addChild(platform)
     }
+    
     func makePlatform6() {
+        let platform = generatePlatform(minY: Int(player.position.y) + 700, maxY: Int(player.position.y) + 900)
+        addChild(platform)
+    }
+    
+    func generateNewPlatform() {
+        // Riduci lo spawn rate delle piattaforme
+        if arc4random_uniform(3) == 0 {
+            return
+        }
+        
+        let minY = Int(lastGeneratedPlatformY) + 200
+        let maxY = minY + 100
+        let platform = generatePlatform(minY: minY, maxY: maxY)
+        lastGeneratedPlatformY = platform.position.y
+        addChild(platform)
+    }
+    
+    func generatePlatform(minY: Int, maxY: Int) -> SKSpriteNode {
         let platform = SKSpriteNode(imageNamed: "Platform")
-        platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: 800, highestValue: 900).nextInt() + Int(player.position.y))
+        platform.position = CGPoint(x: GKRandomDistribution(lowestValue: Int(size.width * 0.1), highestValue: Int(size.width * 0.9)).nextInt(), y: GKRandomDistribution(lowestValue: minY, highestValue: maxY).nextInt())
         platform.zPosition = 5
+        platform.setScale(2)
         platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
         platform.physicsBody?.isDynamic = false
         platform.physicsBody?.allowsRotation = false
@@ -194,6 +187,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         platform.physicsBody?.categoryBitMask = bitmasks.platform.rawValue
         platform.physicsBody?.collisionBitMask = 0
         platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
-        addChild(platform)
+        return platform
     }
 }
