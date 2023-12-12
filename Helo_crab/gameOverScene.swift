e8753270e3caf9d8f4981b3484baf996e360a0b5
XlSolver <salvatoreflauto0@gmail.com>
2023-12-12 12:18:56 +0100

    Tutorial 4 completed
    Add score and best score value + storage for best scores + game over functionality

---
+++ Helo_crab/gameOverScene.swift
@@ -0,0 +1,45 @@
+//
+//  gameOverScene.swift
+//  Helo_crab
+//
+//  Created by Salvatore Flauto on 11/12/23.
+//
+
+import Foundation
+import SpriteKit
+
+class gameOverScene: SKScene {
+    let background = SKSpriteNode(imageNamed: "")
+    let gameOver = SKSpriteNode(imageNamed: "")
+    
+    override func didMove(to view: SKView) {
+        
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(background)
+        
+        gameOver.position = CGPoint(x: size.width / 2, y: size.width / 2)
+        gameOver.setScale(1.6)
+        gameOver.zPosition = -1
+        addChild(gameOver)
+        
+        let tapLabel = SKLabelNode()
+        tapLabel.fontName = "SF Pro"
+        tapLabel.position = CGPoint(x: size.width / 2, y: size.height / 4)
+        tapLabel.text = "Tap to restart"
+        tapLabel.fontSize = 46
+        tapLabel.fontColor = .black
+        
+        let outAction = SKAction.fadeOut(withDuration: 0.5)
+        let inAction = SKAction.fadeIn(withDuration: 0.5)
+        let sequence = SKAction.sequence([outAction, inAction])
+        
+        tapLabel.run(SKAction.repeatForever(sequence))
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let gameScene = GameScene(size: size.self)
+        let transition = SKTransition.flipVertical(withDuration: 0.5)
+        
+        view?.presentScene(gameScene, transition: transition)
+    }
+}
